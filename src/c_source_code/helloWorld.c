9cf3e52d6b66b3385e18b04d0d350d0798d3cc78
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-20 16:11:17 -0400

    Hello World

---
+++ src/c_source_code/helloWorld.c
@@ -0,0 +1,13 @@
+/*
+	helloWorld.c Source Code
+	Written by Jose Escobar Mejia
+	Date: March 20, 2016 @ 4:09.00 PM
+*/
+
+#include <stdio.h>
+
+int main(){
+	// Testing Repo purpose
+	printf("%s\n", "Hello World");
+	return(0);
+}

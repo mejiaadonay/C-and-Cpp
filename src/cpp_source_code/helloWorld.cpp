0eb4086d17eff45ebe9343ea75aa50218fb387ca
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-20 16:15:15 -0400

    Hello World

---
+++ src/cpp_source_code/helloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+	helloWorld.cpp Source Code
+	Written by Jose Escobar Mejia
+	Date: March 20, 2016 @ 4:10.00 PM
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	// Testing Repo purpose
+	cout << "Hello World!" << endl;
+	return 0;
+}

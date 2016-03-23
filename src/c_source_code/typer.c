6a4bef164383205d7075dee0d7f6933a1b80f8fa
Adonay Mejia <mejiaadonay@gmail.com>
2016-03-23 15:26:41 -0400

    Work inside out

--- src/c_source_code/typer.c
+++ src/c_source_code/typer.c
@@ -1,11 +1,17 @@
+/*
+	typer.c @ Source Code
+	Written by: Jose Escobar Mejia
+	Date: 3:21.00 PM
+*/
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	char ch;
 
 	puts("Start typing");
 	puts("Press ~ then enter to stop");
-
+	/*
+	char ch;
 	for(;;)
 	{
 		ch = getchar();
@@ -14,7 +20,10 @@ int main() {
 			break;
 		}
 	}
-
+	*/
+	while(getchar() != '~');	// Working inside out with the getchar
+								// This is equivalent to the commented code above and you saved to declare 	
+								// the char varibale since no data is stored.
 	printf("Thanks!\n");
 	return(0);
 }
